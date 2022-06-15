c8568633bf79b4f50a3dd05eb76afad166da948f
michaelkrause19940107 <92281427+michaelkrause19940107@users.noreply.github.com>
2022-06-15 23:13:20 +0900

    ちょっと待って

---
+++ _20220615_km.py
@@ -0,0 +1,7 @@
+import csv
+
+csv_file = open("./TEST_STOCK.csv", "r", encoding="ms932", errors="", newline="" )
+#リスト形式
+f = csv.reader(csv_file, delimiter=",", doublequote=True, lineterminator="\r\n", quotechar='"', skipinitialspace=True)
+#辞書形式
+f = csv.DictReader(csv_file, delimiter=",", doublequote=True, lineterminator="\r\n", quotechar='"', skipinitialspace=True)
